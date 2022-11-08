e452a51df068e02e61da9b3aed8a20fbb1a43fd3
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-11-08 12:39:54 +0100

    Update auth.py
    more structured + added region

--- modules/auth.py
+++ modules/auth.py
@@ -12,70 +12,81 @@ class TLSAdapter(HTTPAdapter):
         self.poolmanager = PoolManager(num_pools=connections, maxsize=maxsize, block=block,
                                        ssl_version=PROTOCOL_TLSv1_2)
 
-def Auth(line):
-    user_agent = 'RiotClient/60.0.3.4751956.4749685 rso-auth (Windows;10;;Professional, x64)'
-    username = line.split(":")[0].replace('\n', '')
-    password = line.split(":")[1].replace('\n', '')
-    region = line.split(":")[2].replace('\n', '')
-    headers = OrderedDict({
-        "Accept-Language": "en-US,en;q=0.9",
-        "Accept": "application/json, text/plain, */*",
-        'User-Agent': user_agent
-    })
-    session = sesh()
-    session.headers = headers #type:ignore
-    session.mount('https://', TLSAdapter())
-    data = {
-        "client_id": "play-valorant-web-prod",
-        "nonce": "1",
-        "redirect_uri": "https://playvalorant.com/opt_in",
-        "response_type": "token id_token",
-        'scope': 'account openid',
-    }
-    headers = {
-        'Content-Type': 'application/json',
-        'User-Agent': user_agent
-    }
-    r = session.post(f'https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
-    data = {
-        'type': 'auth',
-        'username': username,
-        'password': password
-    }
-    r2 = session.put('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
-    data = r2.json()
-    if "access_token" in r2.text:
-        pattern = compile(
-            'access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
-        data = pattern.findall(data['response']['parameters']['uri'])[0]
-        token = data[0]
+class RiotClient:
+    def __init__(self, line):
+        self.username = line.split(":")[0].replace('\n', '')
+        self.password = line.split(":")[1].replace('\n', '')
 
-    elif "auth_failure" in r2.text:
-        print("banned")
-    else:
-        ver_code = c.input('[b][green]2FA Auth Enabled[/green][/b]. Enter the verification code: \n')
-        authdata = {
-            'type': 'multifactor',
-            'code': ver_code,
+    def get_needs(self, username, password):
+        user_agent = 'RiotClient/60.0.3.4751956.4749685 rso-auth (Windows;10;;Professional, x64)'
+        headers = OrderedDict({
+            "Accept-Language": "en-US,en;q=0.9",
+            "Accept": "application/json, text/plain, */*",
+            'User-Agent': user_agent
+        })
+        session = sesh()
+        session.headers = headers #type:ignore
+        session.mount('https://', TLSAdapter())
+        data = {
+            "client_id": "play-valorant-web-prod",
+            "nonce": "1",
+            "redirect_uri": "https://playvalorant.com/opt_in",
+            "response_type": "token id_token",
+            'scope': 'account openid',
         }
-        r = session.put('https://auth.riotgames.com/api/v1/authorization', json=authdata, headers=headers)
-        data = r.json()
-        if "access_token" in r.text:
-            pattern = compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
+        headers = {
+            'Content-Type': 'application/json',
+            'User-Agent': user_agent,
+        }
+        r = session.post(f'https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
+        data = {
+            'type': 'auth',
+            'username': username,
+            'password': password
+        }
+        r2 = session.put('https://auth.riotgames.com/api/v1/authorization', json=data, headers=headers)
+        data = r2.json()
+        if "access_token" in r2.text:
+            pattern = compile(
+                'access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
             data = pattern.findall(data['response']['parameters']['uri'])[0]
             token = data[0]
-        elif "auth_failure" in r.text:
-            print("Banned")
-        else:
-            print('Failed')
-    headers = {
-        'User-Agent': user_agent,
-        'Authorization': f'Bearer {token}',
-    }
-    r = session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={})
-    entitlement = r.json()['entitlements_token']
-    r = session.post('https://auth.riotgames.com/userinfo', headers=headers, json={})
-    data = r.json()
-    puuid = data['sub']
+            id_token = data[1]
 
-    return [token, entitlement, puuid, region]
+        elif "auth_failure" in r2.text:
+            pass
+        else:
+            ver_code = c.input('[b][green]2FA Auth Enabled[/green][/b]. Enter the verification code: \n')
+            authdata = {
+                'type': 'multifactor',
+                'code': ver_code,
+            }
+            r = session.put('https://auth.riotgames.com/api/v1/authorization', json=authdata, headers=headers)
+            data = r.json()
+            if "access_token" in r.text:
+                pattern = compile('access_token=((?:[a-zA-Z]|\d|\.|-|_)*).*id_token=((?:[a-zA-Z]|\d|\.|-|_)*).*expires_in=(\d*)')
+                data = pattern.findall(data['response']['parameters']['uri'])[0]
+                token = data[0]
+                id_token = data[1]
+            elif "auth_failure" in r.text:
+                pass
+            else:
+               pass
+        headers = {
+            'User-Agent': user_agent,
+            'Authorization': f'Bearer {token}',
+        }
+        r = session.post('https://entitlements.auth.riotgames.com/api/token/v1', headers=headers, json={})
+        entitlement = r.json()['entitlements_token']
+        r = session.post('https://auth.riotgames.com/userinfo', headers=headers, json={})
+        data = r.json()
+        puuid = data['sub']
+
+        reg_json = {'id_token': id_token}
+        r = session.put('https://riot-geo.pas.si.riotgames.com/pas/v1/product/valorant',json=reg_json, headers=headers)
+        region = r.json()['affinities']['live']
+
+        return token, entitlement, puuid, region
+
+    def auth(self):
+        return self.get_needs(self.username, self.password)
